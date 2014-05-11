1e095fff7b470a84463711028ca052d29d951ba0
lillchan <californium98@gmail.com>
2014-05-11 15:17:44 -0700

    adding director model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140511220808) do
+ActiveRecord::Schema.define(version: 20140511221716) do
 
   create_table "albums", force: true do |t|
     t.string   "title"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20140511220808) do
   end
 
   create_table "apps", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "directors", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"

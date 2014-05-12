c8d8b59bff1e331e2163ec7500d5099fa60c19a4
lillchan <californium98@gmail.com>
2014-05-11 17:02:36 -0700

    adding devise views and removing picks from devise user table

--- db/migrate/20140511233222_add_picks_to_user.rb
+++
@@ -1,8 +0,0 @@
-class AddPicksToUser < ActiveRecord::Migration
-  def change
-  	add_column :users, :app_id, :integer
-  	add_column :users, :movie_id, :integer
-  	add_column :users, :album_id, :integer
-  	add_column :users, :website_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140511233222) do
+ActiveRecord::Schema.define(version: 20140511222234) do
 
   create_table "albums", force: true do |t|
     t.string   "title"
@@ -58,10 +58,6 @@ ActiveRecord::Schema.define(version: 20140511233222) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "app_id"
-    t.integer  "movie_id"
-    t.integer  "album_id"
-    t.integer  "website_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

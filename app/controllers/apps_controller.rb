0d7b39e084281e47c91d1df3f0729cfb36b79e85
lillchan <californium98@gmail.com>
2014-05-13 20:00:06 -0700

    adding ability to create new app

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,19 +1,33 @@
 class AppsController < ApplicationController
-	before_action :load_app, only: [:edit, :show, :update]
+	before_action :load_app, only: :show
 
 	def index
 		@apps = App.all
 	end
 
+  def new
+    @app = App.new
+  end
+
   def show
   end
 
   def create
+    @app = App.new(safe_app_params)
+    if @app.save
+      redirect_to @app
+    else
+      render 'new'
+    end
   end
 
   private
 
   def load_app
   	@app = App.find(params[:id])
+  end
+
+  def safe_app_params
+    params.require('app').permit(:name)
   end
 end

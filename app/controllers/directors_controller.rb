60f64a2bcc387e238b479fb2dbdc88d4dc555a0c
lillchan <californium98@gmail.com>
2014-05-14 23:32:33 -0700

    adding ability to add new director

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,10 +1,28 @@
 class DirectorsController < ApplicationController
   def index
+  	@directors = Director.all
   end
 
   def show
+  	@director = Director.find(params[:id])
   end
 
   def new
+  	@director = Director.new
+  end
+
+  def create
+  	@director = Director.new(safe_director_params)
+  	if @director.save
+  		redirect_to @director
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+
+  def safe_director_params
+  	params.require('director').permit(:name)
   end
 end

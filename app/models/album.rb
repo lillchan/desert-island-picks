773bb29cbf76441836f8c944af9f83144d670139
lillchan <californium98@gmail.com>
2014-05-15 19:22:16 -0700

    adding validation to album, app, movie, and website

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,6 @@
 class Album < ActiveRecord::Base
+	validates :title, presence: true
+
 	has_many :profiles
 	has_many :users, through: :profiles
 	belongs_to :artist
--- app/models/website.rb
+++ app/models/website.rb
@@ -1,4 +1,6 @@
 class Website < ActiveRecord::Base
+	validates :name, presence: true
+
 	has_many :profiles
 	has_many :users, through: :profiles
 end
